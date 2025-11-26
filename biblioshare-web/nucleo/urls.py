4afdd13b661c01129df4c1f59f398344b91c0441
arkfx <felipe.leal@mail.uft.edu.br>
2025-11-26 20:58:43 -0300

    Update URL routing in biblioshare-web to include a new home view and adjust health check endpoint. Modify base template to link to the new home view and enhance navigation options for authenticated users.

--- biblioshare-web/nucleo/urls.py
+++ biblioshare-web/nucleo/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from django.http import JsonResponse
 from django.urls import include, path
 
+from .views import HomeView
+
 
 def saude(_):
     return JsonResponse({'status': 'ok', 'mensagem': 'API do BiblioShare operacional'})
@@ -11,11 +13,12 @@ def saude(_):
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('saude/', saude, name='saude'),
+    path('', HomeView.as_view(), name='home'),
     path('api/', include('usuarios.urls')),
     path('api/', include('livros.urls')),
     path('', include('usuarios.urls_web')),
     path('', include('livros.urls_web')),
-    path('', saude, name='saude'),
 ]
 
 if settings.DEBUG:
---
+++ biblioshare-web/nucleo/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import redirect
+from django.views.generic import TemplateView
+
+
+class HomeView(TemplateView):
+    template_name = 'landing.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('livros_web:vitrine')
+        return super().dispatch(request, *args, **kwargs)
+
